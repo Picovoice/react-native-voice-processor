bdecd4441fe90aa9dfc54b6744d3966689380552
Ian <ian@picovoice.ai>
2024-02-08 11:13:33 -0800

    try

---
+++ resources/.test/gradle_6.5/copy.sh
@@ -0,0 +1,5 @@
+cp app.build.gradle ../../../example/android/app/build.gradle
+cp build.gradle ../../../example/android/build.gradle
+cp settings.gradle ../../../example/android/settings.gradle
+cp MainActivity.java ../../../example/android/app/src/main/java/ai/picovoice/reactnative/voiceprocessorexample/MainActivity.java
+cp MainApplication.java ../../../example/android/app/src/main/java/ai/picovoice/reactnative/voiceprocessorexample/MainApplication.java
